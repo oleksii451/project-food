d9ea5517cf2811fc6f2d9ec82afce0f8253bab1b
oleksii <oleksij.yaroshenko@gmail.com>
2021-08-22 16:55:20 +0300

    new calc file

--- dist/js/newjs.js
+++ dist/js/newjs.js
@@ -1,34 +1,17 @@
 'use strict';
 
-class Rectangle {
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
-    }
-
-    calcArea() {
-        return this.height * this.width;
-    }
+const user = {
+    name: "tralala",
+    tel: "+888948333",
+    parents: {
+        papa: "oleksii",
+        mama: "marta"
+    },
 }
 
-class ColouredRectangleWithText extends Rectangle {
-    constructor(height, width, bgColour, text) {
-        super(height, width);
-        this.text = text;
-        this.bgColour = bgColour;
-    }
+const clone = JSON.parse(JSON.stringify(user));
 
-    showMyProps() {
-        console.log(`Tekst: ${this.text}, cvet:${this.bgColour}`);
-    }
-}
-
-const ColouredRect = new ColouredRectangleWithText(20, 20, 'blue', 'come on');
-
-ColouredRect.showMyProps();
-console.log(ColouredRect.calcArea());
-
-// const square = new Rectangle(20,20);
-// const long = new Rectangle(300,10);
-//
-// console.log(square.calcArea(), long.calcArea());
+    
+    clone.parents.mama = 'ukakak';
+    console.log(user);
+    console.log(clone);
